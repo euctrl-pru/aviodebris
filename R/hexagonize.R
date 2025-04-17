2643306ea1a288e596a4985919e92a88a8dcee86
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-04-17 21:38:49 +0200

    add hexagonize step

---
+++ R/hexagonize.R
@@ -0,0 +1,67 @@
+#' assign H3 cell index at specified resolution to all trajectory point
+#'
+#' Keep only portions in the BBOX of the study
+#'
+#' @param day the date for the trajectories, it refers to the relevant
+#'            parquet file in `data/trajectories_<YYYY-MM-DD>_resampled_30s.parquet`
+#' @param resolution the H3 resolution
+#'
+#' @export
+#'
+#' @returns a new parquet file such as
+#'          `data/trajectories_<YYYY-MM-DD>_resampled_30s_bbox_res_<resolution>.parquet`
+#'
+hexagonize_traffic <- function(day, resolution = 3) {
+  date <- day |> lubridate::as_date()
+  year <- lubridate::year(date)
+  month <- lubridate::month(date)
+  day <- lubridate::day(date)
+  date <- date |> format("%Y-%m-%d")
+
+  withr::local_envvar(c(TZ = "UTC", ORA_SDTZ = "UTC"))
+  # con <- |>  withr::local_db_connection(DBI::dbConnect(duckdb(), path = ":memory:"))
+  con <- DBI::dbConnect(duckdb::duckdb(), path = ":memory:")
+
+  DBI::dbExecute(
+    con,
+    "SET extension_directory = '/Users/spi/.duckdb/extensions';"
+  )
+  DBI::dbExecute(con, "LOAD H3;")
+  # fmt: skip
+  query <- stringr::str_glue("
+    CREATE OR REPLACE TABLE TRAJECTORY AS
+    SELECT
+      *,
+      ROW_NUMBER() OVER(PARTITION BY flight_id ORDER BY timestamp) AS seq_id,
+      date_part('day',   timestamp) AS day,
+      date_part('month', timestamp) AS month,
+      date_part('year',  timestamp) AS year,
+      date_part('hour',  timestamp) AS hour,
+      h3_h3_to_string(h3_latlng_to_cell(latitude, longitude, {resolution})) AS cell,
+      {resolution} AS h3_resolution
+    FROM
+        'data/trajectories_{date}_resampled_30s.parquet';
+    ALTER TABLE TRAJECTORY DROP COLUMN sequence_id;
+    ALTER TABLE TRAJECTORY RENAME seq_id TO sequence_id;
+    COPY(
+      SELECT
+        *
+      FROM
+        TRAJECTORY
+      WHERE
+        -- filter on bounding box (the one containing all HEX cells containing NM area)
+        -- xmin = -27.78166, ymin = 24.90098,
+        -- xmax =  48.56037, ymax = 72.85065
+        (
+          (-27.78166 <= longitude AND longitude < 48.56037)
+            AND (24.90098 <= latitude AND latitude < 72.85065)
+        )
+        AND (year = {year} AND month = {month} AND day = {day})
+    )
+    TO
+      'data/trajectories_{date}_resampled_30s_bbox_res_{resolution}.parquet'
+    (FORMAT 'parquet')
+    ;")
+
+  DBI::dbExecute(con, query)
+}
