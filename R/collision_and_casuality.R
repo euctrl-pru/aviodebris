a47c098941470e95bd471e1794f4cece6650ebea
Enrico Spinielli <enrico.spinielli@eurocontrol.int>
2025-05-05 10:49:30 +0200

    fix collision prob

--- R/collision_and_casuality.R
+++ R/collision_and_casuality.R
@@ -25,6 +25,23 @@ collision_and_casuality_risk_expectation_hourly <- function(
       aviodebris::weightings_h3_resolution_3_hourly,
       by = c("cell" = stringr::str_glue("h3_resolution_{resolution}"))
     ) |>
+    # per cell per hour per aircraft type
+    dplyr::group_by(
+      .data$year,
+      .data$month,
+      .data$day,
+      .data$hour,
+      .data$cell,
+      .data$aircraft_type
+    ) |>
+    dplyr::sumarise(
+      # take the mean of the half degree values
+      w = mean(.data$w),
+      # just take the mean, but values should all be the same
+      # this is done to keep the column
+      occupancy = mean(.data$occupancy)
+    ) |>
+    dplyr::ungroup() |>
     dplyr::left_join(
       aviodebris::effective_expose_area,
       by = c("aircraft_type" = "icao")
@@ -33,11 +50,29 @@ collision_and_casuality_risk_expectation_hourly <- function(
       eea = dplyr::if_else(is.na(.data$eea), 1000, .data$eea),
       pax = dplyr::if_else(is.na(.data$pax), 10, .data$pax)
     ) |>
-    dplyr::mutate(
-      collision_expectation = .data$w * .data$occupancy * .data$eea,
-      casuality_risk = .data$collision_expectation * .data$pax,
-      .by = c("year", "month", "day", "hour", "cell", "aircraft_type")
+    # per cell per hour per aircraft type
+    dplyr::group_by(
+      .data$year,
+      .data$month,
+      .data$day,
+      .data$hour,
+      .data$cell,
+      .data$aircraft_type
     ) |>
+    dplyr::summarize(
+      collision_expectation = .data$w * .data$occupancy * .data$eea
+    ) |>
+    dplyr::ungroup() |>
+    # per cell hourly
+    dplyr::group_by(
+      .data$year,
+      .data$month,
+      .data$day,
+      .data$hour,
+      .data$cell
+    ) |>
+    dplyr::summarise(collision_expectation = sum(collision_expectation)) |>
+    dplyr::ungroup() |>
     arrow::write_parquet(
       here::here(
         "data",
